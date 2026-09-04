5ca2763caa6ca75b09ab58c7ee332f1a226ef611
Lokgaev <lokgaevm@gmail.com>
2026-09-04 18:17:49 +0500

    First commit

---
+++ internal/config/config.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	AppAddr string
+
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+
+	MaxPages   int
+	MaxAgeDays int
+}
+
+func Load() (Config, error) {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("[CONFIG] .env файл	не найден, использую системные переменные окружения")
+	}
+
+	maxPages, err := strconv.Atoi(os.Getenv("MAX_PAGES"))
+	if err != nil {
+		return Config{}, fmt.Errorf("неверный MAX_PAGES: %w", err)
+	}
+
+	maxAgeDays, err := strconv.Atoi(os.Getenv("MAX_AGE_DAYS"))
+	if err != nil {
+		return Config{}, fmt.Errorf("неверный MAX_AGE_DAYS: %w", err)
+	}
+
+	cfg := Config{
+		AppAddr: os.Getenv("APP_ADDR"),
+
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+
+		MaxPages:   maxPages,
+		MaxAgeDays: maxAgeDays,
+	}
+
+	return cfg, nil
+}
---
+++ internal/model/article.go
@@ -0,0 +1,31 @@
+package model
+
+import "time"
+
+type Article struct {
+	ID int64 `json:"id"`
+
+	ExternalID string `json:"-"`
+
+	TitleTM *string `json:"title_tm"`
+	TitleRU *string `json:"title_ru"`
+	TitleEN *string `json:"title_en"`
+
+	TextTM *string `json:"text_tm"`
+	TextRU *string `json:"text_ru"`
+	TextEN *string `json:"text_en"`
+
+	PostedAt *time.Time `json:"posted_at"`
+
+	ScrapedAt time.Time `json:"scrapped_at"`
+
+	ImgURL *string `json:"img_url"`
+
+	URL string `json:"url"`
+
+	SourceName string `json:"source_name"`
+
+	Published bool `json:"published"`
+
+	Category *string `json:"category"`
+}
